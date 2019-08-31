0f467739e5af96d318fb2b93534f660bf98964d8
Dicky Arya Kesuma <dickyaryakesuma@gmail.com>
2019-08-31 16:24:13 +0700

    restapi created

---
+++ server/classifierModel/apps.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+import html
+import pathlib
+import os
+#
+# class ClassifiermodelConfig(AppConfig):
+#     name = 'classifierModel'
+#
+
+
+# from fast_bert.prediction import BertClassificationPredictor
+# import classifier model
+class ClassifiermodelConfig(AppConfig):
+    name = 'classifierModel'
+    MODEL_PATH = Path("model")
+    BERT_PRETRAINED_PATH = Path("model/uncased_L-12_H-768_A-12/")
+    LABEL_PATH = Path("label/")
+    predictor = BertClassificationPredictor(model_path = MODEL_PATH/"multilabel-emotion-fastbert-basic.bin",
+                                            pretrained_path = BERT_PRETRAINED_PATH,
+                                            label_path = LABEL_PATH, multi_label=True)
