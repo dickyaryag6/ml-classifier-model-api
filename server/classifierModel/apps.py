137ff9f45dde9a5d7a388aac51d8538cfbec8213
Dicky Arya Kesuma <dickyaryakesuma@gmail.com>
2019-08-31 18:48:54 +0700

    Data added

--- server/classifierModel/apps.py
+++ server/classifierModel/apps.py
@@ -1,20 +1,16 @@
-from django.apps import AppConfig
-import html
-import pathlib
-import os
+# from django.apps import AppConfig
+# import html
+# import pathlib
+# import os
 #
+# from model.
+#
+# # code to load our machine learning model,
 # class ClassifiermodelConfig(AppConfig):
 #     name = 'classifierModel'
-#
-
-
-# from fast_bert.prediction import BertClassificationPredictor
-# import classifier model
-class ClassifiermodelConfig(AppConfig):
-    name = 'classifierModel'
-    MODEL_PATH = Path("model")
-    BERT_PRETRAINED_PATH = Path("model/uncased_L-12_H-768_A-12/")
-    LABEL_PATH = Path("label/")
-    predictor = BertClassificationPredictor(model_path = MODEL_PATH/"multilabel-emotion-fastbert-basic.bin",
-                                            pretrained_path = BERT_PRETRAINED_PATH,
-                                            label_path = LABEL_PATH, multi_label=True)
+#     MODEL_PATH = Path("model")
+#     BERT_PRETRAINED_PATH = Path("model/uncased_L-12_H-768_A-12/")
+#     LABEL_PATH = Path("label/")
+#     predictor = BertClassificationPredictor(model_path = MODEL_PATH/"multilabel-emotion-fastbert-basic.bin",
+#                                             pretrained_path = BERT_PRETRAINED_PATH,
+#                                             label_path = LABEL_PATH, multi_label=True)
--- server/classifierModel/views.py
+++ server/classifierModel/views.py
@@ -10,16 +10,75 @@ from rest_framework.response import Response
 from rest_framework import status
 from .apps import WebappConfig
 
+
+from model import NLPModel()
+
+model = NLPModel()
+
+# clf_path = 'lib/models/SentimentClassifier.pkl'
+# with open(clf_path, 'rb') as f:
+#     model.clf = pickle.load(f)
+#
+# vec_path = 'lib/models/TFIDFVectorizer.pkl'
+# with open(vec_path, 'rb') as f:
+#     model.vectorizer = pickle.load(f)
+
+# argument parsing
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
 class call_model(APIView):
 
     def get(self,request):
         if request.method == 'GET':
+                args = parser.parse_args()
+                user_query = args['query']
+                    # vectorize the user's query and make a prediction
+                uq_vectorized = model.vectorizer_transform(
+                    np.array([user_query]))
+                prediction = model.predict(uq_vectorized)
+                pred_proba = model.predict_proba(uq_vectorized)
+                    # Output 'Negative' or 'Positive' along with the score
+                if prediction == 0:
+                    pred_text = 'Negative'
+                else:
+                    pred_text = 'Positive'
 
-            # sentence is the query we want to get the prediction for
-            params =  request.GET.get('sentence')
+                    # round the predict proba value and set to new variable
+                confidence = round(pred_proba[0], 3)
+                    # create JSON object
+                output = {'prediction': pred_text, 'confidence': confidence}
 
-            # predict method used to get the prediction
-            response = ClassifiermodelConfig.predictor.predict(sentence)
+                return output
 
-            # returning JSON response
-            return JsonResponse(response)
+            # # sentence is the query we want to get the prediction for
+            # params =  request.GET.get('sentence')
+            #
+            # # predict method used to get the prediction
+            # response = ClassifiermodelConfig.predictor.predict(sentence)
+            #
+            # # returning JSON response
+            # return JsonResponse(response)
+
+# class PredictSentiment(Resource):
+#     def get(self):
+#         # use parser and find the user's query
+#         args = parser.parse_args()
+#         user_query = args['query']
+#         # vectorize the user's query and make a prediction
+#         uq_vectorized = model.vectorizer_transform(
+#             np.array([user_query]))
+#         prediction = model.predict(uq_vectorized)
+#         pred_proba = model.predict_proba(uq_vectorized)
+#         # Output 'Negative' or 'Positive' along with the score
+#         if prediction == 0:
+#             pred_text = 'Negative'
+#         else:
+#             pred_text = 'Positive'
+#
+#         # round the predict proba value and set to new variable
+#         confidence = round(pred_proba[0], 3)
+#         # create JSON object
+#         output = {'prediction': pred_text, 'confidence': confidence}
+#
+#         return output
